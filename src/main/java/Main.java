af8678baabe414438bdc5068ba21ee171e7afb94
Vitaliy.Filin <vitaliy.filin@rtkit.ru>
2022-06-18 13:52:52 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+public class Main {
+    public static void main(String[] args) throws TelegramApiException {
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+        try {
+            telegramBotsApi.registerBot(new keepPostureBot());
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
---
+++ src/main/java/keepPostureBot.java
@@ -0,0 +1,53 @@
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class keepPostureBot extends TelegramLongPollingBot {
+    public long chat_id;
+    @Override
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            chat_id = update.getMessage().getChatId();
+            SendMessage message = new SendMessage();
+            message.setChatId(String.valueOf(chat_id));
+            message.setText("Этот бот рандомно отправляет тебе уведомление держать осанку");
+            try {
+                execute(message);
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+            new TimTask().run();
+        }
+    }
+
+    class TimTask extends TimerTask {
+        public void run() {
+            Timer timer = new Timer();
+            int delay = (1 + new Random().nextInt(4)) * 100;
+            timer.schedule(new TimTask(), delay);
+            SendMessage message = new SendMessage();
+            message.setChatId(String.valueOf(chat_id));
+            message.setText("Держи осанку");
+            try {
+                execute(message);
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        return "keepposture_bot";
+    }
+
+    @Override
+    public String getBotToken() {
+        return "5334620803:AAHIJVVhey1i-HX5ODS3F9qFRFG_w3C_Scc";
+    }
+}
